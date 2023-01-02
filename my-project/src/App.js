177d4a3cf32101962ebbc43f61f8233d3e381d43
Tobisky <oparajitee4@gmail.com>
2023-01-01 21:04:35 -1200

    updated some files

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import {FaDev} from 'react-icons/fa'
 function App() {
   return (
     <div className="App">
- 
+        <div>
+          <FaDev className='text-cyan-500' size={30}/>
+          <h1 className='text-red-500'>i am the one</h1>
+        </div>
     </div>
   );
 }
