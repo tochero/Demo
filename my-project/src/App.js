cec716526c970dae43624277464cc006101e824a
Tobisky <oparajitee4@gmail.com>
2023-01-01 21:58:14 -1200

    new

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import {FaDev} from 'react-icons/fa'
+import {FaDev, FaCameraRetro} from 'react-icons/fa'
 function App() {
   return (
     <div className="App">
         <div>
           <FaDev className='text-cyan-500' size={30}/>
+          <FaCameraRetro className='text-lime-600' size={40} />
           <h1 className='text-red-500'>i am the one</h1>
         </div>
     </div>
